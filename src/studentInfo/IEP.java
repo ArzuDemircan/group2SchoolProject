a857d475355e05e0a0e3bb69abfb8fa9d952239c
Arzu Demircan <arzukocdemircan@gmail.com>
2020-03-30 14:18:44 -0400

    Classes

---
+++ src/studentInfo/IEP.java
@@ -0,0 +1,50 @@
+package studentInfo;
+
+public class IEP {
+
+	private String aDHD;
+	private String pTSD;
+	private String visualImpairment;
+	private String hearingImpairment;
+	private String learningDisability;
+	private String autism;
+	
+	public String getADHD() {
+		return aDHD;
+	}
+	public void setADHD(String aDHD) {
+		aDHD = aDHD;
+	}
+	public String getPTSD() {
+		return pTSD;
+	}
+	public void setPTSD(String pTSD) {
+		pTSD = pTSD;
+	}
+	public String getVisualImpairment() {
+		return visualImpairment;
+	}
+	public void setVisualImpairment(String visualImpairment) {
+		this.visualImpairment = visualImpairment;
+	}
+	public String getHearingImpairment() {
+		return hearingImpairment;
+	}
+	public void setHearingImpairment(String hearingImpairment) {
+		this.hearingImpairment = hearingImpairment;
+	}
+	public String getLearningDisability() {
+		return learningDisability;
+	}
+	public void setLearningDisability(String learningDisability) {
+		this.learningDisability = learningDisability;
+	}
+	public String getAutism() {
+		return autism;
+	}
+	public void setAutism(String autism) {
+		this.autism = autism;
+	}
+
+
+}
